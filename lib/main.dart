3e06edcb366c8cd6c6c3f22fed5b29623098f7e7
inzmam <inzmamkhan56@gmail.com>
2023-04-08 22:15:56 +0500

    APi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'Screens/StartingPages/PageView.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
@@ -12,5 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: PageViewPage());
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext?  context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
